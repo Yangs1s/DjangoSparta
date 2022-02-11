99260da0ccb7317cbb002caa38659385a4f9c4cc
Yangs1s <sungjinahim@gmail.com>
2022-02-12 00:23:02 +0900

    commit

--- DjangoSparta/settings.py
+++ DjangoSparta/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from typing import List
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-8r#xg+2)%aad8&(6h(rp8tp=*5xizgp5w6q(lpu+z(5mzndt0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
